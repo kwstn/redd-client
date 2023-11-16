ad7587ac476eacb56d6d848bb499f444f4386ea1
kwstn <alex_costi@icloud.com>
2023-11-16 18:53:18 +0000

    Reddit Articles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Article from "./components/Article";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if (res.status != 200)  {
+        console.log("ERROR");
+        return
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      });
+    })
+  }, [subreddit]);
+
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <input type="text" className="input" value="webdev"></input>
       </header>
+      <div className="articles">
+        <Article />
+      </div>
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Article()  {
+    return  (
+        <article>
+            <a href="#">
+                <h3>Title</h3>
+            </a>
+        </article>
+    )
+}
+
+export default Article;
