0246b1b3be83e66bc0edc446357c8fdbbd2df1fa
kwstn <alex_costi@icloud.com>
2023-11-17 17:06:34 +0000

    Reddit Features

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import './App.css';
 function App()  {
   return (
     <>
-      <Header />
-      <main>
-        <Home />
-      </main>
-      <aside>
-        <Subreddits />
-      </aside>
+     
     </>
   );
 }
